3e8fa662fefcf5d0945a536ed37c4e56d21532af
Mateo_Liberatore <83372310+MateoLiberatore@users.noreply.github.com>
2024-07-02 14:54:32 -0300

    parcial

---
+++ parcial/app.py
@@ -0,0 +1,23 @@
+from flask import render_template,request,redirect,url_for
+import config
+from models import Client,Delivery, client_schema, clients_schema, deliveries_schema, delivery_schema
+import crud
+
+
+app = config.connex_app
+app.add_api(config.basedir/'swagger.yml')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/clients', methods=['GET'])
+def clients():
+    clients_data = crud.read_all_clients()
+    if clients_data is None:
+        clients_data = []
+    return render_template('show_clients.html', clients=clients_data)
+
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
